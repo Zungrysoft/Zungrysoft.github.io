6c58369dd345feb003d46f4443ee4860fb941ed3
ZungrySoft <ZungrySoftEntertainment@gmail.com>
2023-01-30 12:08:03 -0800

    Added Programming page and download counter for Escher Dimension

--- src/pages/Project.js
+++ src/pages/Project.js
@@ -1,3 +1,4 @@
+import React,{useState, useEffect} from 'react';
 const images = require.context('../../public/images', true);
 
 function getImage(str) {
@@ -9,7 +10,39 @@ function getImage(str) {
     }
 }
 
+function getEscherDownloads(setEscherDownloads) {
+    fetch('https://api.github.com/repos/ZungrySoft/The-Escher-Dimension/releases')
+        .then(response => response.json())
+        .then(data => {
+            let total = 0
+            for (let release of data) {
+                for (let asset of release.assets) {
+                    total += asset.download_count
+                }
+            }
+            setEscherDownloads(total)
+        });
+}
+
+function dataFormat(str, escherDownloads) {
+    if (str.includes("**escher_downloads**")) {
+        str = str.replace("**escher_downloads**", escherDownloads)
+    }
+    
+    return str
+}
+
 function ProjectPage({ data }) {
+    const [escherDownloads, setEscherDownloads] = useState("over 12000");
+
+    // Validation
+    useEffect(() => {
+        // No sounds to cancel. Set nosound to false
+        if (escherDownloads === "over 12000") {
+            getEscherDownloads(setEscherDownloads)
+        }
+    }, [escherDownloads]);
+
     return (
         <div>
             <h2>{data.title}</h2>
@@ -18,7 +51,7 @@ function ProjectPage({ data }) {
                 <div className="bounding-box">
                     <div>
                         <h4>{val.title}</h4>
-                        <p>{val.description}</p>
+                        <p>{dataFormat(val.description, escherDownloads, setEscherDownloads)}</p>
                         {val.links ? val.links.map((link, index) =>
                             <div>
                                 <a target="blank" rel="noopener noreferrer" href={link.url}>{link.text}</a>
